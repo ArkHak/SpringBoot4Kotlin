c1f1649276c628046b29cd0007d6dde279384fce
Oleg Mysin <7nacu7@gmail.com>
2025-07-28 17:25:24 +0300

    Add Get, Get Param, Post, Put, Delete

--- src/main/kotlin/o/mysin/spring_boot_kotlin_test/HelloWorldController.kt
+++ src/main/kotlin/o/mysin/spring_boot_kotlin_test/HelloWorldController.kt
@@ -1,2 +1,11 @@
 package o.mysin.spring_boot_kotlin_test
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloWorldController {
+
+    @GetMapping
+    fun helloWorld() = "Hello World Oleg!"
+}
---
+++ src/main/kotlin/o/mysin/spring_boot_kotlin_test/QuoteDto.kt
@@ -0,0 +1,7 @@
+package o.mysin.spring_boot_kotlin_test
+
+data class QuoteDto(
+    val id: Long,
+    val content: String,
+    val author: String
+)
